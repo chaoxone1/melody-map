0059240bf312bdbf7c4da9bda7f6497f4c9d8502
michele di stadio <distadiomichele93@gmail.com>
2024-08-29 16:05:59 +0200

    added markers on map

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,11 +1,23 @@
 class BookmarksController < ApplicationController
 
   def index
+    all_favorite_events = current_user.all_favorites.map(&:favoritable).select { |e| e.is_a?(Event) }
     @upcoming_events = current_user.all_favorites.select do |event|
       event.favoritable.date >= Date.today
     end
     @past_events = current_user.all_favorites.select do |event|
       event.favoritable.date < Date.today
+    end
+
+    @geocoded_events = all_favorite_events.select(&:geocoded?)
+
+    @markers = @geocoded_events.map do |event|
+      {
+        lat: event.latitude,
+        lng: event.longitude,
+        info_window_html: render_to_string(partial: "events/info_window", locals: { event: event }),
+        marker_html: render_to_string(partial: "events/marker")
+      }
     end
   end
 
