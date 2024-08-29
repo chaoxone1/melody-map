eb9c15d477dab37c93117cff4eac2cb94097cb66
Kenan Mehic <keno.mehic11@outlook.it>
2024-08-29 15:46:57 +0200

    fixed create

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "events#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Health status route
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :events do
@@ -21,20 +19,12 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :events
-  resources :users
-  resources :bookmarks
-  resources :followers
+  resources :users, only: [:show]
+  resources :bookmarks, only: [:index, :create, :destroy]
+  resources :followers, only: [:index, :create, :destroy]
+
   get "trendsetters", to: "pages#trendsetters_index"
   get "trendsetters/:id", to: "pages#trendsetters_show", as: :trendsetter
-  # config/routes.rb
   get 'myspaces', to: 'pages#myspaces'
-  get 'followers', to: 'followers#index'
-  get 'bookmarks', to: 'bookmarks#index'
-  get 'events', to: 'events#index'
-  get 'trendsetters', to: 'pages#trendsetters_index'
   get 'myspace', to: 'pages#myspace'
-
-  resources :followers, only: [:create, :destroy]
-  resources :users, only: [:show]
 end
