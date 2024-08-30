d03d6b1192c99fa46d40118196291e4a7ba08ba0
Adrien Kropfeld <adrien.kropfeld@googlemail.com>
2024-08-30 15:03:14 +0200

    better version seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -140,18 +140,22 @@ events << Event.create!(
   category: 'Rock',
   latitude: 52.5145,
   longitude: 13.2394
-)
+).tap do |event|
+  event.photo.attach(io: URI.open("https://res.cloudinary.com/doxvqzbhi/image/upload/v1725022253/Rocks_aeofrc.jpg"), filename: 'Rocks_aeofrc.jpg')
+end
 
 events << Event.create!(
-  name: 'Jazz Night in Düsseldorf',
+  name: 'Classic Night in Düsseldorf',
   date: Date.new(2024, 10, 10),
-  description: 'Enjoy smooth jazz with international artists in Düsseldorf.',
+  description: 'Enjoy smooth classic music from the most known. Let you be inspired by the past of good music of the 18th century. From Mozart to Beethovens Symphony',
   location: 'Tonhalle Düsseldorf, Ehrenhof 1, 40479 Düsseldorf, Germany',
   user: users[2],
   category: 'Classic',
   latitude: 51.2342,
   longitude: 6.7731
-)
+).tap do |event|
+  event.photo.attach(io: URI.open("https://res.cloudinary.com/doxvqzbhi/image/upload/sheet-music_ylbzmb.jpg"), filename: 'sheet-music_ylbzmb.jpg')
+end
 
 events << Event.create!(
   name: 'Munich Pop Open Air',
@@ -162,7 +166,9 @@ events << Event.create!(
   category: 'Pop',
   latitude: 48.1486,
   longitude: 11.5675
-)
+).tap do |event|
+  event.photo.attach(io: URI.open("https://res.cloudinary.com/doxvqzbhi/image/upload/v1725022324/1_idXnmA14KO-yVukmm_TR6w_caw98g.jpg"), filename: '1_idXnmA14KO-yVukmm_TR6w_caw98g.jpg')
+end
 
 events << Event.create!(
   name: 'Frankfurt Classical Gala',
@@ -173,7 +179,9 @@ events << Event.create!(
   category: 'Classic',
   latitude: 50.1152,
   longitude: 8.6716
-)
+).tap do |event|
+  event.photo.attach(io: URI.open("https://res.cloudinary.com/doxvqzbhi/image/upload/v1725022390/1_mixpt3aTomNCPC2-cBlmIw_unvgjf.jpg"), filename: '1_mixpt3aTomNCPC2-cBlmIw_unvgjf.jpg')
+end
 
 events << Event.create!(
   name: 'Electronic Beats in Berlin',
@@ -184,11 +192,13 @@ events << Event.create!(
   category: 'Electronic',
   latitude: 52.5105,
   longitude: 13.4348
-)
+).tap do |event|
+  event.photo.attach(io: URI.open("https://res.cloudinary.com/doxvqzbhi/image/upload/v1725022467/1000_F_94450095_p1dBQGXWou3yN2ILhXnhUnNJi0Ism7hR_danf4a.jpg"), filename: '1000_F_94450095_p1dBQGXWou3yN2ILhXnhUnNJi0Ism7hR_danf4a.jpg')
+end
 
-# Create 15 more events similarly...
+
 events << Event.create!(
-  name: 'Hamburg Indie Festival',
+  name: 'Hamburg Pop Concert',
   date: Date.new(2024, 10, 5),
   description: 'Indie bands from across the globe converge in Hamburg for a day of great music.',
   location: 'Elbphilharmonie, Platz der Deutschen Einheit 4, 20457 Hamburg, Germany',
@@ -196,10 +206,12 @@ events << Event.create!(
   category: 'Pop',
   latitude: 53.5413,
   longitude: 9.9840
-)
+).tap do |event|
+  event.photo.attach(io: URI.open("https://res.cloudinary.com/doxvqzbhi/image/upload/v1725022533/download_qh8ppl.jpg"), filename: 'download_qh8ppl.jpg')
+end
 
 events << Event.create!(
-  name: 'Berlin Ska Night',
+  name: 'Berlin Country Night',
   date: Date.new(2024, 9, 25),
   description: 'A lively ska concert in the heart of Berlin.',
   location: 'SO36, Oranienstraße 190, 10999 Berlin, Germany',
@@ -207,7 +219,9 @@ events << Event.create!(
   category: 'Rock',
   latitude: 52.5006,
   longitude: 13.4200
-)
+).tap do |event|
+  event.photo.attach(io: URI.open("https://res.cloudinary.com/doxvqzbhi/image/upload/v1725022612/images_tr6l4s.jpg"), filename: 'images_tr6l4s.jpg')
+end
 
 events << Event.create!(
   name: 'Munich Hip-Hop Party',
@@ -218,7 +232,9 @@ events << Event.create!(
   category: 'Hip-Hop',
   latitude: 48.1561,
   longitude: 11.5255
-)
+).tap do |event|
+  event.photo.attach(io: URI.open("https://res.cloudinary.com/doxvqzbhi/image/upload/v1725022662/media.media.6e386cb0-6202-439e-aac0-0d7e0f97aec4.original1024_ruwr7j.jpg"), filename: 'media.media.6e386cb0-6202-439e-aac0-0d7e0f97aec4.original1024_ruwr7j.jpg')
+end
 
 events << Event.create!(
   name: 'Düsseldorf Folk Evening',
@@ -229,18 +245,22 @@ events << Event.create!(
   category: 'Country',
   latitude: 51.2258,
   longitude: 6.8083
-)
+).tap do |event|
+  event.photo.attach(io: URI.open("https://res.cloudinary.com/doxvqzbhi/image/upload/v1725022729/198289-004-74FAF303_x0zzfc.webp"), filename: '198289-004-74FAF303_x0zzfc.webp')
+end
 
 events << Event.create!(
-  name: 'Frankfurt Blues Festival',
+  name: 'Frankfurt Hip-Hop Festival',
   date: Date.new(2024, 11, 2),
-  description: 'The best blues artists come together for a memorable night in Frankfurt.',
+  description: 'The best HipHop artists come together for a memorable night in Frankfurt.',
   location: 'Batschkapp Frankfurt, Gwinnerstraße 5, 60388 Frankfurt, Germany',
   user: users[6],
   category: 'Classic',
   latitude: 50.1387,
   longitude: 8.7407
-)
+).tap do |event|
+  event.photo.attach(io: URI.open("https://res.cloudinary.com/doxvqzbhi/image/upload/v1725022795/4014699_beste-stimmung-herrschte-beim-groessten-hiphop-open-air-in-nordbayern_16zu9_1BghVO_dU3hWi_cbfld8.jpg"), filename: '4014699_beste-stimmung-herrschte-beim-groessten-hiphop-open-air-in-nordbayern_16zu9_1BghVO_dU3hWi_cbfld8.jpg')
+end
 
 # Continue with 10 more events similarly...
 
