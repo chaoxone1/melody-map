22d0408cbc28d1069be06e3b6a3c670f55b260f5
code ~/.zshrc <danmuller98@gmail.com>
2024-09-02 16:42:18 +0200

    follow buttons replaced by hearts

--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_02_091407) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "information"
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_events_on_user_id"
@@ -81,6 +82,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_02_091407) do
     t.index ["favoritor_id", "favoritor_type"], name: "fk_favorites"
     t.index ["favoritor_type", "favoritor_id"], name: "index_favorites_on_favoritor"
     t.index ["scope"], name: "index_favorites_on_scope"
+  end
+
+  create_table "followers", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "following_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["following_id"], name: "index_followers_on_following_id"
+    t.index ["user_id"], name: "index_followers_on_user_id"
   end
 
   create_table "follows", force: :cascade do |t|
@@ -114,4 +124,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_02_091407) do
   add_foreign_key "bookmarks", "events"
   add_foreign_key "bookmarks", "users"
   add_foreign_key "events", "users"
+  add_foreign_key "followers", "users"
+  add_foreign_key "followers", "users", column: "following_id"
 end
