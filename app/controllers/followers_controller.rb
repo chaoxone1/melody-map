6ab36bddd05f42e749853ca757573b01124cbf4f
michele di stadio <distadiomichele93@gmail.com>
2024-09-05 15:57:43 +0200

    latest fix on follow

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -4,26 +4,14 @@ class FollowersController < ApplicationController
     @user = User.find(params[:id])
     # Logic for following the creator, e.g., adding the creator to the user's followed creators
     Follow.create(user_id:current_user.id, following_id:@user.id)
-    respond_to do |format|
-      format.turbo_stream  do
-        render turbo_stream: turbo_stream.update(:follow_btn, partial: "follow",
-        locals: { user: @user })
-      end
-      format.html { redirect_to events_path, notice: 'User followed successfully.' }
-    end
+    redirect_to trendsetter_path(@user), notice: 'User followed successfully.'
   end
 
   def destroy
     @user = User.find(params[:id])
     # Logic for following the creator, e.g., adding the creator to the user's followed creators
     Follow.find_by(user_id:current_user.id, following_id:@user.id).destroy
-    respond_to do |format|
-      format.turbo_stream  do
-        render turbo_stream: turbo_stream.update(:follow_btn, partial: "follow",
-        locals: { user: @user })
-      end
-      format.html { redirect_to events_path, notice: 'User followed successfully.' }
-    end
+    redirect_to trendsetter_path(@user), notice: 'User unfollowed successfully.'
   end
 
   def index
