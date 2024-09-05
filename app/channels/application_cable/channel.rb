e6ee8b33a3800d334a618c5933cae252736cea09
Adrien Kropfeld <adrien.kropfeld@googlemail.com>
2024-09-05 15:24:45 +0200

    production fix

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,20 +1,4 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
-    identified_by :current_user
-
-    def connect
-      self.current_user = find_verified_user
-      logger.add_tags "ActionCable", "User #{current_user.id}"
-    end
-
-    protected
-
-    def find_verified_user
-      if current_user == env['warden'].user
-        current_user
-      else
-        reject_unauthorized_connection
-      end
-    end
   end
 end
